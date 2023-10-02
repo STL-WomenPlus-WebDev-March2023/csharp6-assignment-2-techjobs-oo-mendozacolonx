6329976279289507483dd7900a08a30b400bd611
xiomaramendoza <mendozacolonx@gmail.com>
2023-10-02 12:28:44 -0500

    task 2 part 2 adding custom equals, gethashcode, and tostring methods

--- TechJobsOOAutoGraded6/PositionType.cs
+++ TechJobsOOAutoGraded6/PositionType.cs
@@ -19,6 +19,24 @@ namespace TechJobsOOAutoGraded6
         }
 
         // TODO: Task 2: Add custom Equals(), GetHashCode(), and ToString() methods.
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        //auto generated
+        public override bool Equals(object? obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id; //&&
+                  // Value == type.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id); //, Value);
+        }
     }
 }
 
